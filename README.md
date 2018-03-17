6599e72da6ca25668a38b771032ef7f0b9defd2d
n0rthf4ce <eric.liu85@gmail.com>
2018-03-17 10:51:28 -0700

    add readme and comments

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # week-4-game
-Star Wars Game
+Play a Star Wars RPG Game! Select your favorite character to begin playing
+
+Instructions:
+* Click on a character card to begin playing.
+* Select an enemy to battle.
+* Click the "Attack!" button to initiate an attack. Your character gets stronger after every attack, but every time you attack you will receive a counter attack
+* If your character's HP goes to 0, you lose. Click "Play Again" to try again.
+* If the Defender's HP goes to 0 they are defeated. Select a new enemy to battle!
+* If you eliminate all the enemies, you win! Click "Play Again" to play again and select a new character.
+
+Made by Eric Liu, March 2018.
+Github: https://github.com/n0rthf4ce
+Email: eric.liu85@gmail.com
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-
+//initiates an array of character objects along with all other necessary variables
 var startingGroup = [
     {
         name: "Luke Skywalker",
@@ -7,7 +7,7 @@ var startingGroup = [
         sound: "assets/sounds/luke.wav",
         hp: 100,
         attack: 5,
-        increment: 10,
+        increment: 12,
         defense: 5
     },
     {
@@ -17,7 +17,7 @@ var startingGroup = [
         sound: "assets/sounds/han.wav",
         hp: 140,
         attack: 10,
-        increment: 5,
+        increment: 6,
         defense: 8
     },
     {
@@ -47,11 +47,13 @@ var startingGroup = [
         sound: "assets/sounds/sidious.wav",
         hp: 150,
         attack: 25,
-        increment: 1,
+        increment: 2,
         defense: 25
     },
 ], characters = [], user = [], enemies = [], defender = [], soundOn = true, lostGame = false, wonGame = false, audio = document.createElement("audio");;
 
+//generates a character card taking in the arguments: 
+//(array of characters, type of character card,id in which character card goes,and whether character card is clickable or not)
 var showChars = function (arr, cardClass, slot, clickable) {
     $(slot).html("");
     for (let i = 0; i < arr.length; i++) {
@@ -69,19 +71,24 @@ var showChars = function (arr, cardClass, slot, clickable) {
     }
 };
 
+//allows user to change the background image
 var backgroundToggle = function () {
     $(".background-btn").on("click", function () {
         var imgName = $(this).attr("id");
         $("body").attr("style", "background: url('assets/images/" + imgName + ".jpg') no-repeat center center fixed;background-size: cover;");
     });
 };
+
+//allows user to turn sound on or off
 var soundToggle = function () {
     $("#sound-toggle").on("click", function () {
         if (soundOn) {
+            $("#sound-header").html("<i class='fas fa-volume-off'></i> Sound Off:");
             $(this).text("Sound On");
             audio.pause();
             soundOn = false;
         } else {
+            $("#sound-header").html("<i class='fas fa-volume-up'></i> Sound On:");
             $(this).text("Sound Off");
             soundOn = true;
         }
@@ -111,6 +118,7 @@ var initiateBattle = function () {
     });
 };
 
+//allows user to select the defender from the enemies list
 var selectDefender = function () {
     $("#enemies").on("click", ".enemy-card", function () {
         if (defender.length == 0 && !lostGame && !wonGame) {
@@ -127,12 +135,13 @@ var selectDefender = function () {
             audio.setAttribute("src", defender[0].sound);
             if (soundOn) { audio.play(); }
         }
-        $("#defender-header").fadeIn();
+        $("#defender-header").fadeIn("slow");
         $("#attack-btn").show();
         $("#enemies-header").text("Enemies Remaining:");
     });
 };
 
+//displays the results of the attack and whether user has won or lost
 var displayResults = function (enemy, hero) {
     $("#attack-display").text("You attacked " + enemy + " for " + user[0].attack + " damage.");
     if (defender.length == 1) {
@@ -154,6 +163,7 @@ var displayResults = function (enemy, hero) {
     }
 }
 
+//initiates an attack when the user clicks the attack button
 var attack = function () {
     $("#attack-btn").on("click", function () {
         if (defender.length == 1 && !lostGame && !wonGame) {
@@ -185,6 +195,7 @@ var attack = function () {
     })
 }
 
+//resets everything to start a new game
 var reset = function () {
     $("#restart-btn").on("click", function () {
         console.log("restarting");
@@ -198,7 +209,7 @@ var reset = function () {
         lostGame = false;
         wonGame = false;
         $("#attack-btn").hide();
-        $("#character-select-header").fadeIn();
+        $("#character-select-header").fadeIn("slow");
         $("#your-character-header").hide();
         $("#enemies-header").hide();
         $("#defender-header").hide();
@@ -215,6 +226,7 @@ var reset = function () {
     });
 }
 
+//executes all functions when the document is ready
 $(document).ready(function () {
     audio.setAttribute("src", "assets/sounds/theme.mp3")
     audio.play();
